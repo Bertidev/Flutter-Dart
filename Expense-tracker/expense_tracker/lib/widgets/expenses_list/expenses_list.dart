9a70c5bff6171808782960170cbe10ea6c330250
Bertidev <lucasvalber2@gmail.com>
2025-08-25 18:07:34 -0300

    Adicionando apps expense-tracker e todo

---
+++ Expense-tracker/expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,28 @@
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpensesList extends StatelessWidget{
+  const ExpensesList({super.key, required this.expenses, required this.onRemoveExpense});
+
+  final void Function(Expense expense) onRemoveExpense;
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(        //builder tells flutter to create a scrollable list, where the items are only created when in view
+      itemCount: expenses.length, 
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]), 
+        background: Container(
+          color: Theme.of(context).colorScheme.error,
+          margin: Theme.of(context).cardTheme.margin,
+          ),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpenseItem(expenses[index]))
+      );  
+  }
+}
